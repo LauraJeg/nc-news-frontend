a24003f5f192e0d9f454ecab86b900c7267abdfa
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-03-14 12:01:34 +0000

    styling

--- src/Components/CommentCard.jsx
+++ src/Components/CommentCard.jsx
@@ -1,10 +1,11 @@
 import Votes from "./Votes";
+import './styling/commentCard.css'
 
 function CommentCard ({comment}) {
-    return (<>
+    return (<div className = 'commentCard'>
         <p>{comment.body}</p>
         <p>{comment.author} Created on {comment.created_at.slice(0,10)}</p>
         <Votes key = {comment.comment_id} votes = {comment.votes} commentOrArticle = 'comment'/>
-    </>)
+    </div>)
 };
 export default CommentCard;
