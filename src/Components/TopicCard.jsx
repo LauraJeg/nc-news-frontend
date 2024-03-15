6298ab200594a323972e7aae0edeb0dfaa5d4fc8
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-03-15 14:42:31 +0000

    added topic link

--- src/Components/TopicCard.jsx
+++ src/Components/TopicCard.jsx
@@ -1,4 +1,9 @@
+import { Link } from "react-router-dom";
+
 function TopicCard ({topic}) {
-    return <p>Hello!</p>
+    return <Link to={`/topics/${topic.slug}`}>
+                <h3 >{topic.slug}</h3>
+                <p>{topic.description}</p>
+            </Link>
 };
 export default TopicCard;
