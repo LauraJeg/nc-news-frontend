e97c253321b0e68701eda983fdacf08cdb8d0756
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-03-17 01:25:53 +0000

    added error handling to ArticlePage

--- src/Components/ArticlePage.jsx
+++ src/Components/ArticlePage.jsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import { getArticleByArticleID } from "../api";
 import AllComments from "./AllComments";
 import Votes from "./Votes";
+import Error from "./Error";
 
 function ArticlePage () {
     const {article_id} = useParams();
     const [article, setArticle]= useState({});
     const [showComments, setShowComments]= useState(false);
+    const [newError, setNewError]= useState([])
 
 
     const [isLoading, setIsLoading] = useState(true);
@@ -18,9 +20,13 @@ function ArticlePage () {
                 setArticle(article);
                 setIsLoading(false);
             })
+            .catch(({response})=> {console.log(response.status, response.data.msg)
+                setIsLoading(false);
+            return setNewError([response.status, response.data.msg])})
     }, []);
 
     if (isLoading) return <p>Loading...</p>
+    else if(newError.status === undefined) return <Error statuscode={newError[0]} msg={newError[1]}/>
 
     return (
         <>
@@ -30,7 +36,6 @@ function ArticlePage () {
             <h3>By {article.author}</h3>
             <p>{article.topic} </p>
             <p>Created at {article.created_at.slice(0,10)}</p>
-            {/* link to article page with topic */}
             <p>{article.body}</p>
             <Votes patchID = {article.article_id} votes = {article.votes} commentOrArticle = 'article'/>
         </div>
