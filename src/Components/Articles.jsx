9803ea6e536ef941a4709f5bccf7048b775c3f35
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-05-23 20:55:20 +0100

    added some error handling

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -8,6 +8,7 @@ function Articles ({currArticles, setCurrArticles}) {
     const [isLoading, setIsLoading] = useState(true);
     const [sortBy, setSortBy] = useState("created_at");
     const [orderBy, setOrderBy] = useState("desc");
+    const [error, setError] = useState(false);
 
     useEffect(()=> {
         setIsLoading(true);
@@ -16,9 +17,16 @@ function Articles ({currArticles, setCurrArticles}) {
                 setCurrArticles(articles.slice(0,10))
                 setIsLoading(false)
             })
+            .catch((error) => {
+                setError(true);
+                setLoading(false);
+              });
     }, [sortBy, orderBy]);
 
     if (isLoading) return <p>Loading...</p>
+    if (error) {
+        return <NotFound />;
+      }
 
     return ( <>
         <SortByAndOrderBy orderBy={orderBy} setOrderBy={setOrderBy} setSortBy={setSortBy} sortBy={sortBy}/>
